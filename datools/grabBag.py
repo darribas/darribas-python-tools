7ad2c4c2917b24bbe10d87ae157cf467892f12c4
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2012-08-15 19:03:58 +0200

    Adding copy_shp method

--- datools/grabBag.py
+++ datools/grabBag.py
@@ -3,6 +3,17 @@ Different utils with no common topic, just to make your life easier when you
 work with data
 '''
 
+def copy_shp(shp_in, shp_out):
+    '''
+    Copy files from a shapefile other than the dbf
+    '''
+    cmd = 'cp %s %s'%(shp_in, shp_out)
+    os.system(cmd)
+    cmd = 'cp %s %s'%(shp_in.replace('shp', 'shx'), shp_out.replace('shp', 'shx'))
+    os.system(cmd)
+    cmd = 'cp %s %s'%(shp_in.replace('shp', 'prj'), shp_out.replace('shp', 'prj'))
+    os.system(cmd)
+    return None
 
 def fixXLScsv(file):
     '''
